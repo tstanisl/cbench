7e90c71c689016cb1159d41f85ae2c96e72b2f4b
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2022-05-23 23:11:10 +0200

    basic code and test

--- include/cbench.h
+++ include/cbench.h
@@ -1,1 +1,52 @@
 #pragma once
+
+#include <time.h>
+
+typedef struct {
+	size_t left;
+	size_t count;
+	double min_time;
+	double unit_time;
+	struct timespec ts;
+} cbench;
+
+static inline
+cbench* cbench_start(cbench *b) {
+	b->count = 0;
+	b->left = 1;
+	return b;
+}
+
+static inline
+int cbench__cold_path(cbench *b) {
+	struct timespec toc;
+	clock_gettime(CLOCK_MONOTONIC, &toc);
+	if (b->count == 0) { // warm-run
+		b->ts = toc;
+		b->count = b->left = 1;
+		return 0;
+	}
+
+	struct timespec tic = b->ts;
+	double elapsed = (toc.tv_sec - tic.tv_sec) + 1e-9 * (toc.tv_nsec - tic.tv_nsec);
+	if (elapsed < b->min_time) {
+		b->count *= 10;
+		b->left = b->count - 1;
+		clock_gettime(CLOCK_MONOTONIC, &b->ts);
+		return 0;
+	}
+
+	b->unit_time = elapsed / b->count;
+
+	return 1;
+}
+
+static inline
+int cbench_is_done(bench *b) {
+	if (b->left-- != 0) // hot-path
+		return 0;
+	return cbench__cold_patch(b);
+}
+
+#define CBENCH_LOOP(b) \
+	for (cbench* _cbench = cbench_start(b); !cbench_done(_cbench); )
--- tests/sleep.c
+++ tests/sleep.c
@@ -1,5 +1,21 @@
 #include <cbench.h>
 
+#include <unistd.h>
+
 int main(void) {
+	cbench b = { .min_time = 1 };
+	const int usec = 50 * 1000;
+
+	CBENCH_LOOP(&b) {
+		usleep(usec);
+	}
+
+	printf("%f: ", b.unit_time);
+	if (0.95 * usec < b.unit_time && b.unit_time < 1.05 * usec)
+		puts("- passed");
+	else
+		puts("- failed");
+
+
 	return 0;
 }
